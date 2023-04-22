710056b7f4962fd5230b513349fd196ebb25aab6
Luyando <130085596+Lu-the-Zed-man@users.noreply.github.com>
2023-04-22 18:42:34 +0200

    Add files via upload

---
+++ Grules.js
@@ -0,0 +1,28 @@
+let scenario = []
+//The grammar rules and assigning the give fuction its function.
+function give() {
+    var rules = {
+        "start": "#[character:#animal#]story#",
+        "adj": ["brave", "ugly", "tedious", "cunning", "trecherous"],
+        "name": ["Luyando", "Paul", "Abel", "Christopher", "Hassan", "Moses"],
+        "verb": ["sung", "fought", "wept"],
+        "adv": ["loudly", "quietly", "swiftly", "slowly", "faintly"],
+        "animal": ["frog", "cow", "chief", "nyami-nyami"],
+        "place": ["feild", "mountain", "hill", "bush site", "trench"],
+        "time": ["evening", "day"],
+        "story": ["The #adj# #character# #verb# #adv# and #adv# left the scene. Later on in the #time#, the #character# met with #name# at the #place# to discuss about the earlier event."]
+    }
+    //Calling the tracery function to create the grammar we just defined and generating some end result from it.
+    let grammar = tracery.createGrammar(rules);
+    let expansion = grammar.flatten('#start#');
+
+    scenario.push(expansion);
+    printScenario(scenario);
+}
+function printScenario(scenario) {
+    let textBox = document.getElementById("scenario");
+    textBox.innerHTML = "";
+    for(let i=scenario.length-1; i>=0; i--) {
+        textBox.innerHTML += "<p>"+scenario[i]+"</p>"
+    }
+}
